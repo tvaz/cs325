78dc5db82ad52b65e5c31f2f5b94d81735e15b24
Cody <ckagawa@gmail.com>
2016-04-04 14:37:35 -0700

    forgot to add wifi

---
+++ final/wifi/Packet.java
@@ -0,0 +1,40 @@
+package wifi;
+
+class Packet {
+	
+	public Packet(byte[] src) {
+		// TODO Auto-generated constructor stub
+	}
+	public static byte[] generatePacket(byte[] src, short dest, short home, short type, boolean retry, short seq)
+	{
+		return null;
+	}
+	
+	public short getSqnc()
+	{
+		//return sequence #
+		return 0;
+	}
+	public boolean getRetry()
+	{
+		//retry bit
+		return false;
+	}
+	public short getType()
+	{
+		//frame type
+		return 0;
+	}
+	public short getDestAddr()
+	{
+		return 0;
+	}
+	public short getSrcAddr()
+	{
+		return 0;
+	}
+	public byte[] getData()
+	{
+		return null;
+	}
+}
